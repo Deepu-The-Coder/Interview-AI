be70f17111af2a7e68f46e6e44fcec2ea8181ea3
Your Name <deepakburari05@gmail.com>
2026-05-13 20:47:28 +0530

    cors fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,7 +14,9 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin:process.env.FRONTEND_URL,
-    credentials:true
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE"],
+    allowedHeaders:["Content-Type","Authorization"]
 }))
 
 
