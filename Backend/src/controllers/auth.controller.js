a57d40209a3815a09c49942e0744be14157d620d
Your Name <deepakburari05@gmail.com>
2026-05-15 00:50:33 +0530

    testing mode and changed frontend to local

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -48,12 +48,13 @@ async function registerUserController(req,res){
         {expiresIn:"1d"}
     )
     //set token to cookies
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none",
-        maxAge:24 * 60 * 60 * 1000
-    })
+    // res.cookie("token",token,{
+    //     httpOnly:true,
+    //     secure:true,
+    //     sameSite:"none",
+    //     maxAge:24 * 60 * 60 * 1000
+    // })
+    res.cookie("token", token)
 
     res.status(201).json({
         message: "User registered successfully",
@@ -96,12 +97,13 @@ async function loginUserController(req,res){
         {expiresIn:"1d"}
     )
     //do this so that user remains logged in while refresh
-    res.cookie("token",token,{
-        httpOnly:true,
-        secure:true,
-        sameSite:"none",
-        maxAge:24 * 60 * 60 * 1000
-    })
+    // res.cookie("token",token,{
+    //     httpOnly:true,
+    //     secure:true,
+    //     sameSite:"none",
+    //     maxAge:24 * 60 * 60 * 1000
+    // })
+    res.cookie("token", token)
     res.status(200).json({
         message:"User loggedIn successfully.",
         user:{
